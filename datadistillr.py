ee8885d23d49ade34bdc930d4aa7e2381c2c055a
ForrestRogers-datadistillr <86311094+ForrestRogers-datadistillr@users.noreply.github.com>
2021-06-23 23:22:00 -0700

    Update datadistillr.py

--- datadistillr.py
+++ datadistillr.py
@@ -1,14 +1,22 @@
+import requests
+
 class datadistillr:
-    def datadistillr():
-            import requests
 
-            login = "https://devapp.datadistillr.io/api/login"
-            logininfo = {"email":"forrest@datadistillr.com","password":"abcd1234"}
-            logout = "https://devapp.datadistillr.io/api/logout"
-            projects = "https://devapp.datadistillr.io/api/projects"
+    def __init__(self):
+        self.login_page = "https://devapp.datadistillr.io/api/login"
+        self.logout_page = "https://devapp.datadistillr.io/api/logout"
+        self.projects_page = "https://devapp.datadistillr.io/api/projects"
 
-            requests.post(url = login,params = logininfo)
-            requests.get(url = logout)
-            requests.get(url = projects)
+
+    def login(self, username, password):
+        user_info = {"email": str(username), "password": str(password)}
+        requests.post(url = self.login_page, params = user_info)
+
+    def logout(self):
+        requests.get(url = self.logout_page)
+
+    def get_projects(self):
+        requests.get(url = self.projects_page)
+
 
 
